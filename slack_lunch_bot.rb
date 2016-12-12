e453bdaacb222131cf17b8aae6b9807bc04fefa6
Josua Schmid <josua.schmid@renuo.ch>
2016-12-12 21:33:49 +0100

    Require ENVs

--- slack_lunch_bot.rb
+++ slack_lunch_bot.rb
@@ -1,10 +1,5 @@
-# frozen_string_literal: true
+ENV.update YAML.load_file('config/application.yml')
+
 require 'slack-ruby-bot'
 require 'slack_lunch_bot/commands/lunch'
 require 'slack_lunch_bot/bot'
-
-begin
-  ENV.update YAML.load_file('config/application.yml')
-rescue
-  {}
-end
