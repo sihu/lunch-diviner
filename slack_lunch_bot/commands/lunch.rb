9c39eb7ae40ccd271b7f8a2c7d7e1cb8adec7659
Josua Schmid <josua.schmid@renuo.ch>
2016-12-13 08:59:57 +0100

    Add users statically

--- slack_lunch_bot/commands/lunch.rb
+++ slack_lunch_bot/commands/lunch.rb
@@ -7,15 +7,15 @@ module SlackLunchBot
     class Lunch < SlackRubyBot::Commands::Base
       command 'reishauer?' do |client, data, _match|
         a = LunchDiviner.new
-        client.message text: a.slack_formatted_menu, channel: data.channel
+        client.message(text: a.slack_formatted_menu, channel: data.channel)
       end
 
       command 'groups?' do |client, data, _match|
-        p client
-        p data
-        users = %w(blub schmijos)
+        users = %w(benjaminboehm felixkubli matthias pandermatt rbuetzer samuel schmijos lunchbot alessandro alicia
+                   alyssa amina digi martin nick rroger sihu stephane zora)
+
         g = Groupador.new(users)
-        client.message text: g.slack_formatted_groups(2), channel: data.channel
+        client.message(text: g.slack_formatted_groups(6), channel: data.channel)
       end
     end
   end
