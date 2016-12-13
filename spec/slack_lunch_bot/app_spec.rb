5121d50d694cbb766d031b290ddd2b8e454a694f
Josua Schmid <josua.schmid@renuo.ch>
2016-12-13 08:59:43 +0100

    Shorten spec

--- spec/slack_lunch_bot/app_spec.rb
+++ spec/slack_lunch_bot/app_spec.rb
@@ -1,10 +1,7 @@
+# frozen_string_literal: true
 require 'spec_helper'
 
 describe SlackLunchBot::App do
-  def app
-    SlackLunchBot::App.new
-  end
-
   context 'authenticated' do
     it_behaves_like 'a slack ruby bot'
   end
