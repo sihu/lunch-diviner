f9563f0328af7176814e80ec272a893a5ae69657
Josua Schmid <josua.schmid@renuo.ch>
2016-12-20 14:07:37 +0100

    React to future "stephanes"

--- slack_lunch_bot/commands/lunch.rb
+++ slack_lunch_bot/commands/lunch.rb
@@ -4,6 +4,10 @@ require_relative '../../lib/lunch_diviner'
 module SlackLunchBot
   module Commands
     class Lunch < SlackRubyBot::Commands::Base
+      command 'I miss you!' do |client, data, _match|
+        client.message text: "That's an unilateral statement...", channel: data.channel
+      end
+
       command 'reishauer?' do |client, data, _match|
         a = LunchDiviner.new
         client.message text: a.slack_formatted_menu, channel: data.channel
